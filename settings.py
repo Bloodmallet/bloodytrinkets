14fbe4cb6f92809b4868924d78376d353d1e8a6e
pkaskaround <kruse.peter.1990@gmail.com>
2017-02-18 05:17:20 +0100

    - format all the things

--- settings.py
+++ settings.py
@@ -2,10 +2,26 @@
 ## 
 ## 
 
-graph_colours = { "865": "#4572a7", "875": "#aa4643", "885": "#89a54e", "895": "#71588f", "905": "#4198af", "915": "#db843d", "925": "#00E676" }
+graph_colours = { 
+  "865": "#4572a7", 
+  "875": "#aa4643", 
+  "885": "#89a54e", 
+  "895": "#71588f", 
+  "905": "#4198af", 
+  "915": "#db843d", 
+  "925": "#00E676" 
+}
 graph_name = "Icefury trinket sims 7.1.5"
 # Defines itemlevels that shall be simed ordered from highest to lowest (graph output will have this order reversed)
-ilevels = [ "925", "915", "905", "895", "885", "875", "865" ]
+ilevels = [ 
+  "925", 
+  "915", 
+  "905", 
+  "895", 
+  "885", 
+  "875", 
+  "865"
+]
 
 output_screen = False
 # "json", "highchart"
