d7aff5b61c14f50fa1cea0f8cf281c12796c3076
pkaskaround <kruse.peter.1990@gmail.com>
2017-07-20 13:52:17 +0200

    allows multiple fight styles and target error for each

--- automator.py
+++ automator.py
@@ -2,88 +2,98 @@ import datetime
 import subprocess
 import sys
 
+fight_styles = [ 
+  ( "patchwerk", "0.1" ), 
+  ( "hecticaddcleave", "0.2" ) 
+]
+
 profiles = [
-  ( "Death Knight - Frost - Patchwerk", "death_knight", "frost" ),
-  ( "Death Knight - Unholy - Patchwerk", "death_knight", "unholy" ),
-  ( "Demon Hunter - Havoc - Patchwerk", "demon_hunter", "havoc" ),
-  ( "Druid - Balance - Patchwerk", "druid", "balance" ),
-  ( "Druid - Feral - Patchwerk", "druid", "feral" ),
-  ( "Hunter - Beast Mastery - Patchwerk", "hunter", "beast_mastery" ),
-  ( "Hunter - Marksmanship - Patchwerk", "hunter", "marksmanship" ),
-  ( "Hunter - Survival - Patchwerk", "hunter", "survival" ),
-  ( "Mage - Arcane - Patchwerk", "mage", "arcane" ),
-  ( "Mage - Fire - Patchwerk", "mage", "fire" ),
-  ( "Mage - Frost - Patchwerk", "mage", "frost" ),
-  ( "Monk - Windwalker - Patchwerk", "monk", "windwalker" ),
-  ( "Paladin - Retribution - Patchwerk", "paladin", "retribution" ),
-  ( "Priest - Shadow - Patchwerk", "Priest", "shadow" ),
-  ( "Rogue - Assassination - Patchwerk", "rogue", "assassination" ),
-  ( "Rogue - Outlaw - Patchwerk", "rogue", "outlaw" ),
-  ( "Rogue - Subtlety - Patchwerk", "rogue", "subtlety" ),
-  ( "Shaman - Elemental - Patchwerk", "shaman", "elemental" ),
-  ( "Shaman - Enhancement - Patchwerk", "shaman", "enhancement" ),
-  ( "Warlock - Affliction - Patchwerk", "warlock", "affliction" ),
-  ( "Warlock - Demonology - Patchwerk", "warlock", "demonology" ),
-  ( "Warlock - Destruction - Patchwerk", "warlock", "destruction" ),
-  ( "Warrior - Arms - Patchwerk", "warrior", "arms" ),
-  ( "Warrior - Fury - Patchwerk", "warrior", "fury" )
+  ( "death_knight", "frost" ),
+  ( "death_knight", "unholy" ),
+  ( "demon_hunter", "havoc" ),
+  ( "druid", "balance" ),
+  ( "druid", "feral" ),
+  ( "hunter", "beast_mastery" ),
+  ( "hunter", "marksmanship" ),
+  ( "hunter", "survival" ),
+  ( "mage", "arcane" ),
+  ( "mage", "fire" ),
+  ( "mage", "frost" ),
+  ( "monk", "windwalker" ),
+  ( "paladin", "retribution" ),
+  ( "priest", "shadow" ),
+  ( "rogue", "assassination" ),
+  ( "rogue", "outlaw" ),
+  ( "rogue", "subtlety" ),
+  ( "shaman", "elemental" ),
+  ( "shaman", "enhancement" ),
+  ( "warlock", "affliction" ),
+  ( "warlock", "demonology" ),
+  ( "warlock", "destruction" ),
+  ( "warrior", "arms" ),
+  ( "warrior", "fury" )
 ]
 
 start = datetime.datetime.now()
 
-for profile in profiles:
-  with open("automator_input.py", "w") as ofile:
-    ofile.write("graph_title = \"" + profile[0] + "\"\n")
-    ofile.write("simc_settings = {}\n")
-    ofile.write("simc_settings[\"class\"] = \"" + profile[1] + "\"\n")
-    ofile.write("simc_settings[\"spec\"] = \"" + profile[2] + "\"\n")
+for fight_style in fight_styles:
+  for profile in profiles:
+    with open("automator_input.py", "w") as ofile:
+      ofile.write("graph_title = \"" + profile[0].title() + " - " + profile[1].title() + " - " + fight_style[0].title() + "\"\n")
+      ofile.write("simc_settings = {}\n")
+      ofile.write("simc_settings[\"class\"] = \"" + profile[0] + "\"\n")
+      ofile.write("simc_settings[\"spec\"] = \"" + profile[1] + "\"\n")
+      ofile.write("simc_settings[\"fight_style\"] = \"" + fight_style[0] + "\"\n")
+      ofile.write("simc_settings[\"target_error\"] = \"" + fight_style[1] + "\"\n")
 
-  print("")
+    print("")
 
-  command = "python bloodytrinkets.py"
-  if sys.platform == 'win32':
-    # call simulationcraft in the background. grab output for processing and get dps value
-    startupinfo = subprocess.STARTUPINFO()
-    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    command = "python bloodytrinkets.py"
+    if sys.platform == 'win32':
+      # call bloodytrinkets in the background
+      startupinfo = subprocess.STARTUPINFO()
+      startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
-    result = subprocess.run(
-      command, 
-      stdout=None, 
-      stderr=subprocess.DEVNULL, 
-      universal_newlines=True, 
-      startupinfo=startupinfo
-    )
-    while result.returncode != 0:
-      print(result)
-      print("I keep trying")
       result = subprocess.run(
         command, 
         stdout=None, 
         stderr=subprocess.STDOUT, 
-        universal_newlines=True,
+        universal_newlines=True, 
         startupinfo=startupinfo
       )
+      while result.returncode != 0:
+        print(result)
+        print("I keep trying")
+        result = subprocess.run(
+          command, 
+          stdout=None, 
+          stderr=subprocess.STDOUT, 
+          universal_newlines=True,
+          startupinfo=startupinfo
+        )
 
-  else:
-    result = subprocess.run(
-      command, 
-      stdout=None, 
-      stderr=subprocess.DEVNULL, 
-      universal_newlines=True
-    )
-    while result.returncode != 0:
+    else:
       result = subprocess.run(
         command, 
         stdout=None, 
         stderr=subprocess.STDOUT, 
         universal_newlines=True
       )
+      while result.returncode != 0:
+        result = subprocess.run(
+          command, 
+          stdout=None, 
+          stderr=subprocess.STDOUT, 
+          universal_newlines=True
+        )
 end = datetime.datetime.now()
 print( "Done after " + str( end - start ))
 
 
-## Use this in settings to automate the process
+## Add this to settings to automate the process
 # import automator_input
-# graph_name = automator_input.graph_name
+# graph_title = automator_input.graph_title
 # simc_settings["class"] = automator_input.simc_settings["class"]
 # simc_settings["spec"]  = automator_input.simc_settings["spec"]
+# simc_settings["fight_styles"] = automator_input.simc_settings["fight_styles"]
+# simc_settings["target_error"] = automator_input.simc_settings["target_error"]
